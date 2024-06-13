ec975f9deca43728b1ff82415381cc2ed2a13262
Dee-pik <Deepikaraja70@gmail.com>
2024-06-13 18:28:43 +0530

    TestData.java

--- Week3Day1/TestData.java
+++ Week3Day1/TestData.java
@@ -1,12 +1,12 @@
 package Week3Day1;
 
-public class TestData {
+public class TestData {//it is super class 
 	
-	 public void enterCredentials() {
+	 public void enterCredentials() {//created the method for creds
 		 System.out.println("This is a methods from parent class - Tesdata - entercreds");
 	}
 	
-	 public void  navigateToHomePage() {
+	 public void  navigateToHomePage() {//created the method for navigated to home page
 		 System.out.println("This method is from parent class - navigate to homepage");
 	 }
 	

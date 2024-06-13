150aeadfa8f667d84c5e66b02e29caf8a8af13f2
Dee-pik <Deepikaraja70@gmail.com>
2024-06-13 18:27:13 +0530

    LoginTestData.java

--- Week3Day1/LoginTestData.java
+++ Week3Day1/LoginTestData.java
@@ -1,16 +1,16 @@
 package Week3Day1;
 
-public class LoginTestData extends TestData {
+public class LoginTestData extends TestData {//inherited from parent class
 	
-	public void enterUserName() {
+	public void enterUserName() {//created a method for enter username
       System.out.println("This method is from child class - enter username : ");	
 	}
-	public void enterPassword() {
+	public void enterPassword() {//created a method for enterPassWord
 		System.out.println("This method is from child class - enter password:");
 	}
       public static void main (String [] args) {
     	  
-    	  LoginTestData out  = new LoginTestData();
+    	  LoginTestData out  = new LoginTestData();//creating a object to call tge method and inherit the method from other class
     	  
     	  out.enterCredentials();
     	  out.navigateToHomePage();
